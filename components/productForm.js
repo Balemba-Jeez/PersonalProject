6543220a32aaa29ba62032943128ca593451f381
balembajessy <balembajessy@gmail.com>
2025-05-02 04:43:52 +0100

    Finish cloud upload products

--- components/productForm.js
+++ components/productForm.js
@@ -1,22 +1,24 @@
 import Layout from "@/components/Layout";
 import { data } from "autoprefixer";
 import axios from "axios";
+import { set } from "mongoose";
 import { redirect } from "next/dist/server/api-utils";
 import { useRouter } from "next/router";
 import {useState} from "react";
 
 
-export default function ProductForm({_id, title:existingTitle, description:existingDescription,price:existingPrice, images}){
+export default function ProductForm({_id, title:existingTitle, description:existingDescription,price:existingPrice, images:existingImages}){
 
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
     const [price, setPrice] = useState(existingPrice || '');
+    const [images, setImages] = useState(existingImages || []);
     const [goToProducts, setGoToProducts] = useState(false);
     const router = useRouter();
 
     async function saveProduct(ev) {
         ev.preventDefault();
-        const data = {title,description,price};
+        const data = {title,description,price, images};
 
         if (_id){
             //update
@@ -40,11 +42,10 @@ export default function ProductForm({_id, title:existingTitle, description:exist
                 data.append('file', file)
             }
  
-            const  res = await fetch('/api/upload', {
-                method: 'POST',
-                body: data,
+            const  res = await axios.post('/api/upload', data)
+            setImages(oldImages => {
+                return [...oldImages, ...res.data.links]
             })
-            console.log(res);
         }
     }
     return(
@@ -58,7 +59,12 @@ export default function ProductForm({_id, title:existingTitle, description:exist
                     value={title} 
                     onChange={ev => setTitle(ev.target.value)}/>
                 <label >Photos</label>
-                <div className="mb-2">
+                <div className="mb-2 flex flex-wrap gap-2">
+                    {!!images?.length && images.map(link => (
+                        <div key={link}  className="h-24 ">
+                            <img src={link} alt="" className="rounded-lg"/>
+                        </div>
+                    ))}
                     <label className="w-24  h-24 cursor-pointer text-center text-sm gap-1 rounded-lg bg-gray-200 text-gray-500 flex  items-center justify-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
